f795be1922002a7a2cfb04b6ce71a08118566390
ysz20011009 <165889830+ysz20011009@users.noreply.github.com>
2026-05-04 10:11:33 +0800

    Update mfflnet.py

--- mmaction/models/backbones/mfflnet.py
+++ mmaction/models/backbones/mfflnet.py
@@ -442,7 +442,7 @@ class MFFLNet(BaseModule):
         
         x = torch.cat([
             self.class_embedding.to(x.dtype) + torch.zeros(
-                x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device), x
+                1, x.shape[1], x.shape[-1], dtype=x.dtype, device=x.device), x
         ],
             dim=0)  # shape = [grid ** 2 + 1, *, width]
         x = x + self.positional_embedding.to(x.dtype).unsqueeze(1)
